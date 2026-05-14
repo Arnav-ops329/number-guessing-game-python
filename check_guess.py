cedce701a5d63ddadf706ed6e93a5049a80b8d07
Arnav-ops329 <arnavmaitrey44@gmail.com>
2026-05-14 15:20:59 +0530

    Fixed return issue

--- check_guess.py
+++ check_guess.py
@@ -1,7 +1,10 @@
 def check_guess(user_guess, computer_guess):
     if user_guess > computer_guess:
         print("Try smaller ones.")
+        return False
     elif user_guess < computer_guess:
         print("Try greater ones.")
+        return False
     else:
         print("YOU GUESSES IT RIGHT !")
+        return True
