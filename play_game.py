62b1c267ea95407f623807d0c8c3e1ba082db47e
Arnav-ops329 <arnavmaitrey44@gmail.com>
2026-05-14 14:37:46 +0530

    Add logic in play_game.py

---
+++ play_game.py
@@ -0,0 +1,17 @@
+import welcome
+import number_generation
+import user_input
+import check_guess
+
+
+def play_game():
+    welcome.welcome()
+    computer_guess = number_generation.veronica_number_guess()
+    chances = 0
+    while chances < 0:
+        user_guess = user_input.get_user_input()
+
+        check_guess.check_guess(user_guess, computer_guess)
+        chances += 1
+    if chances == 10:
+        print("0 GUESS LEFT, VERONICA WON")
