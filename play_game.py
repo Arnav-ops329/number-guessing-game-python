905a1afd76af6ff2100fdd67890838b2e2bed3df
Arnav-ops329 <arnavmaitrey44@gmail.com>
2026-05-14 15:25:08 +0530

    Improved the result logic

--- play_game.py
+++ play_game.py
@@ -8,10 +8,15 @@ def play_game():
     welcome.welcome()
     computer_guess = number_generation.veronica_number_guess()
     chances = 0
+    won = False
     while chances < 10:
         user_guess = user_input.get_user_input()
 
-        check_guess.check_guess(user_guess, computer_guess)
+        result = check_guess.check_guess(user_guess, computer_guess)
         chances += 1
-    if chances == 10:
-        print("0 GUESS LEFT, VERONICA WON")
+        if result:
+            won = True
+            print("YOU WON")
+            break
+    if not won:
+        print("0 CHANCES LEFT, VERONICA WON.")
